c4f574d39abd3046824d88069225253018653770
Yash <yash.agg2000@gmail.com>
2021-09-17 11:09:22 +0530

    Final Commit

--- App.js
+++ App.js
@@ -1,35 +1,46 @@
 import React, {useState} from 'react';
 import {
   View,
-  Text,
   StyleSheet,
-  TextInput,
   Button,
-  ScrollView,
-  FlatList
+  FlatList,
 } from 'react-native';
 
 import ListItem from './src/components/ListItem';
 import TaskInput from './src/components/TaskInput';
 
 const App = () => {
-  
   const [todoList, setTodoList] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
 
-
-  const handleList = (taskTitle) => {
+  const handleList = taskTitle => {
     setTodoList([...todoList, taskTitle]);
+    setModalVisible(false);
   };
-  const deleteTask = (taskIndex) => {
-    setTodoList(todoList.filter((task, index) => index !== taskIndex))
-  }
+  const deleteTask = taskIndex => {
+    setTodoList(todoList.filter((task, index) => index !== taskIndex));
+  };
+  const cancelInputHandler = () => {
+    setModalVisible(false);
+  };
 
   return (
     <View style={styles.container}>
-      <TaskInput onAddTask={handleList}/>
+      <Button title="Add new Task" onPress={() => setModalVisible(true)} />
+      <TaskInput
+        visible={modalVisible}
+        onAddTask={handleList}
+        onCancel={cancelInputHandler}
+      />
       <FlatList
         data={todoList}
-        renderItem={itemData => <ListItem index={itemData.index} onDelete={deleteTask} title={itemData.item}/> }
+        renderItem={itemData => (
+          <ListItem
+            index={itemData.index}
+            onDelete={deleteTask}
+            title={itemData.item}
+          />
+        )}
       />
     </View>
   );
@@ -39,7 +50,6 @@ const styles = StyleSheet.create({
   container: {
     padding: 40,
   },
-  
 });
 
 export default App;
--- src/components/TaskInput.js
+++ src/components/TaskInput.js
@@ -1,39 +1,61 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, Button, TextInput} from 'react-native';
+import {StyleSheet, View, Button, TextInput, Modal} from 'react-native';
 
-const TaskInput = (props) => {
-    const [enteredTask, setEnteredTask] = useState('');
+const TaskInput = props => {
+  const [enteredTask, setEnteredTask] = useState('');
 
-    const handleTask = enteredText => {
-        setEnteredTask(enteredText);
-    };
+  const handleTask = enteredText => {
+    setEnteredTask(enteredText);
+  };
+  const clearInput = () => {
+    props.onAddTask(enteredTask);
+    setEnteredTask('');
+  };
 
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        placeholder="Enter the task"
-        style={styles.input}
-        value={enteredTask}
-        onChangeText={handleTask}
-      />
-      <Button title="ADD" onPress={props.onAddTask.bind(this, enteredTask)} />
-      {/* <Button title="ADD" onPress={() => props.onAddTask(enteredTask)} /> */}
-    </View>
+    <Modal visible={props.visible} animationType="slide">
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Enter the task"
+          style={styles.input}
+          value={enteredTask}
+          onChangeText={handleTask}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="CANCEL" color="red" onPress={props.onCancel} />
+          </View>
+          <View style={styles.button}>
+            <Button title="ADD" onPress={clearInput} />
+            {/* <Button title="ADD" onPress={() => props.onAddTask(enteredTask)} /> */}
+          </View>
+        </View>
+      </View>
+    </Modal>
   );
 };
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-      },
-    input: {
-        borderColor: '#000',
-        borderWidth: 1,
-        width: '80%',
-        padding: 10,
-      },
+  inputContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    borderColor: '#000',
+    borderWidth: 1,
+    width: '80%',
+    padding: 10,
+    marginBottom: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '50%',
+  },
+  button: {
+    width: '40%',
+  },
 });
 
 export default TaskInput;
